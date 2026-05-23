64623f999700a320dd451de626afa05ee1277df7
Ilham Meidi <ilham.meidi@gmail.com>
2026-05-24 01:10:56 +0700

    input detection, player movement, stamina, camera

---
+++ Tugas1/Assets/Game/Player/PlayerStamina.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PlayerStamina : MonoBehaviour
+{
+    [SerializeField] 
+    private float _maxStamina = 100; 
+
+    [SerializeField] 
+    private float _sprintStaminaCost = 20; 
+
+    [SerializeField] 
+    private float _staminaRegenValue = 20;
+
+    [SerializeField] 
+    private PlayerCharacterMovement _characterMovement; 
+
+    private float _currentStamina; 
+
+    public void CalculateStamina() 
+    { 
+        if (_characterMovement.IsSprint) 
+        { 
+            if (_currentStamina > 0) 
+            { 
+             _currentStamina = _currentStamina - _sprintStaminaCost * Time.deltaTime; 
+            } 
+            else 
+            { 
+                _characterMovement.SetSprint(false); 
+            } 
+        } 
+        else 
+        { 
+            _currentStamina = _currentStamina + _staminaRegenValue * Time.deltaTime; 
+        } 
+        _currentStamina = Mathf.Clamp(_currentStamina, 0, _maxStamina); 
+    } 
+
+    void Awake()
+    {
+        _currentStamina = _maxStamina;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateStamina(); 
+    }
+}
---
+++ Tugas1/Assets/Game/Scripts/InputManager.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine; 
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static GameInputAction;
+
+public class InputManager : MonoBehaviour, IPlayerActions
+{
+    private GameInputAction _inputAction;
+    public UnityEvent<Vector2> OnMoveInput; 
+    public UnityEvent<bool> OnSprintInput; 
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        OnMoveInput?.Invoke(context.ReadValue<Vector2>()); 
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        Debug.Log("Interaksi"); 
+    }
+    
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.performed) 
+        { 
+            OnSprintInput?.Invoke(true); 
+        } 
+        // Mengecek apakah input dilepas 
+        if (context.canceled) 
+        { 
+            OnSprintInput?.Invoke(false); 
+        } 
+    }
+
+    private void Awake()
+    {
+        _inputAction = new GameInputAction();
+        _inputAction.Enable();
+        _inputAction.Player.Enable();
+        _inputAction.Player.SetCallbacks(this); 
+    }
+
+    private void OnDisable()
+    {
+        _inputAction.Player.Disable();
+        _inputAction.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        _inputAction.Player.SetCallbacks(null);
+        _inputAction.Dispose();
+    }
+} 
